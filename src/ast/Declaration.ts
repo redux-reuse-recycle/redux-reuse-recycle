4ff6eec80543788a1f53c8d81f5c90ab7204bf39
kizggerg <gregory_gzik@acl.com>
2019-09-29 18:44:54 -0700

    Add Declaration Node

--- src/ast/Declaration.ts
+++ src/ast/Declaration.ts
@@ -1,5 +1,17 @@
 import ASTNode from "./ASTNode";
+import Type from "./Type";
+import Identifier from "./Identifier";
+import Value from "./Value";
 
 export default class Declaration extends ASTNode {
+  public readonly type: Type;
+  public readonly id: Identifier;
+  public readonly value: Value;
 
+  constructor(type: Type, id: Identifier, value: Value) {
+    super();
+    this.type = type;
+    this.id = id;
+    this.value = value;
+  }
 }
--- src/ast/Value.ts
+++ src/ast/Value.ts
@@ -1,4 +1,6 @@
 import ASTNode from "./ASTNode";
+import Type from "./Type";
+import { Identifier } from "typescript";
 
 export default abstract class Value extends ASTNode {
 
