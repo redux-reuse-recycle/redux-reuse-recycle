6f7969cbcadaa345c1b0c2391f13c16c494d80d2
Jeffrey Doyle <jeffreydoylec@gmail.com>
2019-09-29 22:26:29 -0700

    Added action codegen tests

--- src/codegen/generators/ActionFileGenerator.ts
+++ src/codegen/generators/ActionFileGenerator.ts
@@ -1,5 +1,6 @@
 import AbstractFileGenerator from "./AbstractFileGenerator";
 import ActionsNode from '../ir/ActionsNode';
+import ActionInterface from "../interfaces/ActionInterface";
 
 class ActionFileGenerator extends AbstractFileGenerator {
     private actionNode: ActionsNode;
@@ -9,8 +10,45 @@ class ActionFileGenerator extends AbstractFileGenerator {
         this.actionNode = actionNode;
     }
 
+    private generateConstants(): string {
+        let constants: string = '';
+
+        this.actionNode.actions.forEach((action: ActionInterface) => {
+            constants += `const ${action.type} = "${action.type}";\n`;
+        });
+
+        return constants;
+    }
+
+
+    private formatFunctionName(actionType: string): string {
+        let functionName: string = "";
+        const actionTypesArray = actionType.split("_");
+        actionTypesArray.forEach((word: string, index: number) => {
+            let firstLetter = '';
+            if (index > 0) firstLetter = word.charAt(0).toUpperCase();
+            else firstLetter = word.charAt(0).toLowerCase();
+            const otherLetters = word.substring(1, word.length).toLowerCase();
+            functionName += firstLetter + otherLetters;
+        });
+        return functionName;
+    }
+
+    private generateActionCreators(): string {
+        let functions: string = '';
+
+        console.log('generateActionCreators');
+
+        this.actionNode.actions.forEach((action: ActionInterface) => {
+            functions += `const ${this.formatFunctionName(action.type)} = (payload) => ({\n` +
+                    `\ttype: ${action.type},\n` + '\tpayload\n' + '});\n';
+        });
+
+        return functions;
+    }
+
     codeGen(): string {
-        return '';
+        return `${this.generateConstants()}\n${this.generateActionCreators()}`;
     }
 }
 
---
+++ test/ActionFileGenerator.spec.ts
@@ -0,0 +1,47 @@
+import 'mocha';
+import { expect } from "chai";
+
+import Logger from "../src/utils/Logger";
+Logger.LogToConsole = false;
+
+import ActionFileGenerator from "../src/codegen/generators/ActionFileGenerator";
+import ActionsNode from "../src/codegen/ir/ActionsNode";
+import FileCreator from '../src/utils/FileCreator';
+
+describe("ActionFileGenerator",  () => {
+    it("Produces the correct output for three actions", () => {
+        let node = new ActionsNode('USERS');
+        node.addAction({type: 'GET_ALL_USERS', hasPayload: false});
+        node.addAction({type: 'OPEN_MODAL', hasPayload: false});
+        node.addAction({type: 'SET_USER_LOGGED_IN', hasPayload: false});
+        let code = new ActionFileGenerator(node).codeGen();
+        expect(code).to.equal('const GET_ALL_USERS = "GET_ALL_USERS";\n' +
+            'const OPEN_MODAL = "OPEN_MODAL";\n' +
+            'const SET_USER_LOGGED_IN = "SET_USER_LOGGED_IN";\n' +
+            '\n' +
+            'const getAllUsers = (payload) => ({\n' +
+            '\ttype: GET_ALL_USERS,\n' +
+            '\tpayload\n' +
+            '});\n' +
+            'const openModal = (payload) => ({\n' +
+            '\ttype: OPEN_MODAL,\n' +
+            '\tpayload\n' +
+            '});\n' +
+            'const setUserLoggedIn = (payload) => ({\n' +
+            '\ttype: SET_USER_LOGGED_IN,\n' +
+            '\tpayload\n' +
+            '});\n')
+    })
+
+    it("Produces the correct output for one actions", () => {
+        let node = new ActionsNode('USERS');
+        node.addAction({type: 'GET_ALL_USERS', hasPayload: false});
+        let code = new ActionFileGenerator(node).codeGen();
+        expect(code).to.equal('const GET_ALL_USERS = "GET_ALL_USERS";\n' +
+            '\n' +
+            'const getAllUsers = (payload) => ({\n' +
+            '\ttype: GET_ALL_USERS,\n' +
+            '\tpayload\n' +
+            '});\n')
+    })
+});
