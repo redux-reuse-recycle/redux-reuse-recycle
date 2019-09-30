54b27748c62bf87e083c27c42369248c5fee08c9
kizggerg <gregory_gzik@acl.com>
2019-09-29 20:07:49 -0700

    Fix main

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import Logger from "./utils/Logger";
 // Example of import ast nodes (for Brian).
 import * as AST from "./ast";
 const myVar = new AST.Program();
-*
+*/
 
 export const main = () => {
   Logger.Log("Hello World!");
