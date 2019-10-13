5a0b77a4f471aa299eea9cc00aab7b45c3364aaa
Brian Neumann <c2i0b@ugrad.cs.ubc.ca>
2019-10-13 13:45:34 -0700

    Integrate IR/Codegen Visitors

--- src/Main.ts
+++ src/Main.ts
@@ -2,26 +2,45 @@ import FileReader from "./utils/FileReader";
 import Tokenizer from "./Tokenizer";
 import ParserVisitor from "./visitor/ParserVisitor";
 import TypeCheckVisitor from "./visitor/TypeCheckVisitor";
-import { ASTNode } from "./ast";
+import * as AST from "./ast";
+import IRGenVisitor from "./visitor/IRGenVisitor";
+import SymbolTable from "./symbol_table/SymbolTable";
+import ProgramNode from "./codegen/ir/ProgramNode";
+import CodegenVisitor from "./visitor/CodegenVisitor";
 
 export default class Main {
 
   // Parses and Typechecks the FloScript File
   // and generates its Redux code.
   public static run(filePath: string) {
-    const ast = this.parse(filePath);
-    this.typecheck(ast);
-    // TODO: Connect AST to CodeGen
+    const ast: AST.ASTNode = this.parse(filePath);
+    const table: SymbolTable = this.typecheck(ast);
+    const ir: ProgramNode = this.irgen(ast, table);
+    this.codegen(ir);
   }
 
-  public static parse(filePath: string, fileReader: FileReader = new FileReader()): ASTNode {
+  public static parse(filePath: string, fileReader: FileReader = new FileReader()): AST.ProgramFile {
     const programText = fileReader.ReadProgram(filePath);
     const tokenizer = new Tokenizer(programText, filePath);
     return new ParserVisitor(tokenizer, fileReader).parse();
   }
 
-  public static typecheck(ast: ASTNode): ASTNode {
-    new TypeCheckVisitor().typecheck(ast);
-    return ast;
+  public static typecheck(ast: AST.ASTNode): SymbolTable {
+    const visitor: TypeCheckVisitor = new TypeCheckVisitor();
+    visitor.typecheck(ast);
+    return visitor.getSymbolTable();
   }
+
+  public static irgen(ast: AST.ASTNode, table: SymbolTable): ProgramNode {
+    const visitor: IRGenVisitor = new IRGenVisitor(table);
+      ast.acceptASTVisitor(visitor);
+      return visitor.getProgramNode();
+  }
+
+  public static codegen(ir: ProgramNode) {
+    const visitor: CodegenVisitor = new CodegenVisitor();
+    ir.acceptVisitor(visitor);
+    // Done?
+  }
+
 }
